d9a5a7979dd229e42780e01d805ccc8efdced378
Marcos Moura <eomarcosmoura97@gmail.com>
2024-07-25 17:51:05 -0300

    feat: Criação da pasta components e menu

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,25 @@
 export default function Home() {
   return (
     <>
-    <h1>TITULO</h1>
+      <div className="menu">
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+        </div>
+
+        <nav className="menu-desktop">
+          <ul>
+            <li>
+              <a href="">Idiomas</a>
+            </li>
+            <li>
+              <a href="">Projeto</a>
+            </li>
+            <li>
+              <a href="">Contatos</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </>
-  )
+  );
 }
