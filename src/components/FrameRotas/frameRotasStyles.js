bdd95a573df30c93bde7c8d87753e2afa94026ca
SaulBotelho <saulogabriel_05@hotmail.com>
2024-08-01 20:57:52 -0300

    fizz:Ajustes no componente frameRotas.

--- src/components/FrameRotas/frameRotasStyles.js
+++ src/components/FrameRotas/frameRotasStyles.js
@@ -1,32 +1,102 @@
 import styled from "styled-components";
 export const Container = styled.div`
+  padding-bottom: 30px;
+
   .frame {
-    height: 220px;
-    width: 850px;
-    background-color: #e2dfdf;
+    position:relative;
     display: flex;
-    padding: 15px 0px 0px;
-    justify-content: center;
-    max-width: 100%;
-    margin: 0 300px 0 210px;
-    border-radius: 8px;
-    margin-top: 2px;
-    margin-bottom: 5px;
-    align-items: center;
-    
-  }
-  .frame h3 {
-    margin: 0px 200px 160px 200px;
-    color: #04136f;
-    font-weight: 600;
-  }
-  .frame .cardCidade{
-    display: flex;
-    height: 150px;
-  }
-  .frame .perfilGuia{
-    height: 50px;
-    display: flex;
+    background-color: #E2DFDF;
+    width: 950px;
+    height: 240px;
+    padding: 15px 25px 50px 35px;
+    margin: 0 300px 0 210px; 
+    /* align-items: center; */
+    border-radius: 7px;
 
   }
-`;
+
+.imagem-fundo {
+  position: relative;
+  display: flex; /* Remove espaços em branco em torno da imagem */
+  align-items: center;
+  width:auto;
+  height: 30vh;
+  bottom: 10px;
+  top: 4px;
+}
+
+.imagem-sobreposta {
+  display: flex;
+  position: absolute;
+  height: 65px; /* Mantém a proporção da imagem sobreposta */
+  top: 20px;
+  left: 188px;
+
+}
+.textosFrame h3 {
+  white-space: nowrap;
+  /* overflow: hidden; */
+  /* text-overflow: ellipsis; */
+  width: 200px;
+  position: absolute;
+  top: 40px;
+  left: 31%; 
+  color: #04136F;
+  font-weight: bold;
+  font-size: 20px;
+}
+.nomeGuia{
+  color:#000;
+  position: relative;
+  top: 60px;
+  left: 5%;
+}
+ .estrela{
+  position: absolute;
+  top: 0px;
+  left: 15%;
+  box-shadow: 3px 2px 2px #000 ;
+  border-radius: 8px;
+  background-color: transparent;
+}
+.nota{
+  position: absolute;
+  top: 0px;
+  left: 18%;
+  font-size: 12px;
+  color: #636363; 
+}
+.avaliacao{
+  position: relative;
+  top: -14px;
+  left: 40%;
+  font-size: 12px;
+  color: #636363;
+
+}
+
+.paragrafo {
+  top: 80px;
+  position: relative;
+  left: 5%;
+  font-size: 17px;y
+}
+.valor{
+color: #04136F;
+position: relative;
+left: 86%;
+top: 32px;
+
+}
+.detalheIcones{
+  display: flex;
+  position: absolute;
+  align-items: center;
+  flex-direction: row;
+  font-size: 11px;
+  max-width: 100%;
+  gap: 20px;
+  left: -255px;
+  bottom: -30px;
+}
+`
--- src/components/FrameRotas/framerotas.jsx
+++ src/components/FrameRotas/framerotas.jsx
@@ -1,20 +1,162 @@
 import { Container } from "./frameRotasStyles";
 import CardRecife from '../../assets/cardRecifeFrame.svg'
 import PerfilGuia from '../../assets/fotoPerfilGuia.svg'
+import { Clock, FlagPennant, Star, UserSound } from "@phosphor-icons/react";
 
 export default function FrameRotas(){
   return (
     <Container>
       <section className="frame">
         <div className="cardCidade">
-          <img src={CardRecife} alt="" />
+          <img src={CardRecife} alt="" class="imagem-fundo" />
+          <img src={PerfilGuia} alt="" class="imagem-sobreposta"/>
         </div>
-        <div className="perfilGuia">
-          <img src={PerfilGuia} alt="" />
-        </div>
-        <div className="tituloFrame">
+        
+        <div className="textosFrame">
           <h3>Tour pelo Recife antigo.</h3>
+
+          <div className="nomeGuia">
+            <h5>Jailton Silva</h5>
+
+            <Star className="estrela" size={12} color="#f1f500" weight="fill" />
+
+            <div className="nota">
+              <p>4.2</p>
+
+              <p className="avaliacao">164 avaliações</p>
+            </div>
+          </div>
+
+          <div className= "paragrafo">
+          <p>
+            Conheça a historia de Recife essa cidade com a terceira rua mais belas do Mundo,
+            conhecendo suas origens e evolução enquanto exploramos as ruas dessa cidade maravilhosa.
+           </p>
+
+          <div className="valor">
+            <h2>R$ 98</h2>
+          </div>
+
+          <div className="detalheIcones">
+          <Clock size={18} color="#636363" weight="bold" />
+            <p className="horas">
+              4 Horas
+            </p>
+            <UserSound size={18} color="#363636" />
+            <p className="idioma">
+              Português/Inglês
+            </p>
+            <FlagPennant size={18} color="#363636" /> 
+            <p className="visitaGuiada">
+              Visitas guiadas
+            </p>
+          </div>
+
         </div>
+
+        </div> 
+      </section>
+
+      <section className="frame">
+        <div className="cardCidade">
+          <img src={CardRecife} alt="" class="imagem-fundo" />
+          <img src={PerfilGuia} alt="" class="imagem-sobreposta"/>
+        </div>
+        
+        <div className="textosFrame">
+          <h3>Tour pelo Recife antigo.</h3>
+
+          <div className="nomeGuia">
+            <h5>Jailton Silva</h5>
+
+            <Star className="estrela" size={12} color="#f1f500" weight="fill" />
+
+            <div className="nota">
+              <p>4.2</p>
+
+              <p className="avaliacao">164 avaliações</p>
+            </div>
+          </div>
+
+          <div className= "paragrafo">
+          <p>
+            Conheça a historia de Recife essa cidade com a terceira rua mais belas do Mundo,
+            conhecendo suas origens e evolução enquanto exploramos as ruas dessa cidade maravilhosa.
+           </p>
+
+          <div className="valor">
+            <h2>R$ 98</h2>
+          </div>
+
+          <div className="detalheIcones">
+          <Clock size={18} color="#636363" weight="bold" />
+            <p className="horas">
+              4 Horas
+            </p>
+            <UserSound size={18} color="#363636" />
+            <p className="idioma">
+              Português/Inglês
+            </p>
+            <FlagPennant size={18} color="#363636" /> 
+            <p className="visitaGuiada">
+              Visitas guiadas
+            </p>
+          </div>
+
+        </div>
+
+        </div> 
+      </section>
+
+      <section className="frame">
+        <div className="cardCidade">
+          <img src={CardRecife} alt="" class="imagem-fundo" />
+          <img src={PerfilGuia} alt="" class="imagem-sobreposta"/>
+        </div>
+        
+        <div className="textosFrame">
+          <h3>Tour pelo Recife antigo.</h3>
+
+          <div className="nomeGuia">
+            <h5>Jailton Silva</h5>
+
+            <Star className="estrela" size={12} color="#f1f500" weight="fill" />
+
+            <div className="nota">
+              <p>4.2</p>
+
+              <p className="avaliacao">164 avaliações</p>
+            </div>
+          </div>
+
+          <div className= "paragrafo">
+          <p>
+            Conheça a historia de Recife essa cidade com a terceira rua mais belas do Mundo,
+            conhecendo suas origens e evolução enquanto exploramos as ruas dessa cidade maravilhosa.
+           </p>
+
+          <div className="valor">
+            <h2>R$ 98</h2>
+          </div>
+
+          <div className="detalheIcones">
+          <Clock size={18} color="#636363" weight="bold" />
+            <p className="horas">
+              4 Horas
+            </p>
+            <UserSound size={18} color="#363636" />
+            <p className="idioma">
+              Português/Inglês
+            </p>
+            <FlagPennant size={18} color="#363636" /> 
+            <p className="visitaGuiada">
+              Visitas guiadas
+            </p>
+          </div>
+
+        </div>
+
+        </div> 
       </section>
     </Container>
   );
