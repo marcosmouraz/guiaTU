8f1040aa9d4296a50e7b9c55f97af3c4d2c3842b
Marcos Moura <eomarcosmoura97@gmail.com>
2024-08-02 17:08:50 -0300

    fizz: inclusão de imagens svg na pasta assets e ajustes na tela cadastro turista.

--- src/pages/CadastroTurista/cadTurStyles.js
+++ src/pages/CadastroTurista/cadTurStyles.js
@@ -21,22 +21,38 @@ export const Container = styled.body`
     color: #04136e;
   }
 
-
-  .inputfoto{
+  .inputfoto {
+    position: relative;
     display: flex;
     justify-content: end;
     flex-direction: column;
-    width: 65rem;
-    padding-bottom: 8rem;
-    margin-left: 1rem;
-
+    width: 7rem;
+    padding-bottom: 2rem;
+    margin-left: 45rem;
+    text-align: center;
+    align-items: center;
+    gap: 5px;
+    top: -3.9rem;
+    .vetor {
+      border: #1e1e1e;
+      background-color: aliceblue;
+      border-style: groove;
+      border-radius: 10px;
+    }
+    .vetor:hover {
+      border-style: groove;
+      border-color: #04136e;
+      background-color: #04136e;
+    }
   }
-
-  
+  .inputfoto .p {
+    color: #04136e;
+    font-size: 16px;
+    font-weight: 600;
+  }
   .inputnome {
     display: flex;
     position: relative;
-    /* justify-content: center; */
     flex-direction: row;
     gap: 10px;
     width: 50%;
@@ -44,7 +60,7 @@ export const Container = styled.body`
     padding-left: 4rem;
     top: -2.9rem;
 
-    .nome{
+    .nome {
       width: 100%;
       height: 5vh;
       border-style: groove;
@@ -69,7 +85,7 @@ export const Container = styled.body`
     left: 16rem;
     padding-left: 4rem;
     top: -1.5rem;
-    
+
     .dataNascimento {
       width: 100%;
       height: 5vh;
@@ -113,5 +129,58 @@ export const Container = styled.body`
       padding: 1rem;
       /* margin-left: 1rem; */
     }
+  }
+  .buttonProximo {
+    position: relative;
+    justify-content: center;
+    align-items: center;
+    top: 2.5rem;
+    width: 15%;
+    background-color: #04136e;
+    color: #fff;
+    border-radius: 8px;
+    cursor: pointer;
+    transition: background-color 0.2s;
+    transform: translate(39.7vw, 0.5vh);
+    border: none;
+    text-align: center;
+    display: flex;
+    padding: 8px 6rem;
+  }
+
+  .buttonProximo:hover {
+    color: #fff;
+    background-color: #1e1e1e;
+    border-style: groove;
+    border-color: #fff;
+    border: none;
+  }
+  .linha {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    top: 4.5rem;
+    margin-bottom: 1rem;
+    width: 75rem;
+    padding-bottom: 2rem;
+  }
+  .linha h5 {
+    font-weight: 600;
+    color: #000;
+  }
+  .linha img {
+    width: 20%;
+    color: black;
+  }
+  .sociais {
+    display: flex;
+    gap: 45px;
+    justify-content: center;
+    padding-top: 2.5rem;
+    padding-right: 2.5rem;
+    padding-bottom: 3rem;
+    align-items: center;
   }
 `;
--- src/pages/CadastroTurista/cadastroturista.jsx
+++ src/pages/CadastroTurista/cadastroturista.jsx
@@ -1,6 +1,11 @@
 import { UserFocus } from "@phosphor-icons/react";
 import Menu from "../../components/Menu/menu";
 import { Container } from "./cadTurStyles";
+import Linha from "../../assets/vetorLinha.svg";
+import Google from "../../assets/iconGoogle.svg"
+import Facebook from "../../assets/iconFacebook.svg"
+import Apple from "../../assets/iconApple.svg"
+import Footer from "../../components/Footer/footer";
 
 export default function CadastroTurista() {
   return (
@@ -10,32 +15,54 @@ export default function CadastroTurista() {
       <div className="titulo">
         <h2>Olá Turista, realize seu cadastro...</h2>
       </div>
-      
-      <div className="inputfoto">
-        <p className="p">
-          Escolha sua foto de perfil
-        </p> 
-      <UserFocus className="alteraFoto" size={90} color="#636363" />
-      </div>
+
+      <section className="inputfoto">
+        <p className="p">Escolha sua Foto de Perfil</p>
+        <a href="">
+          <div className="alterafoto">
+            <UserFocus className="vetor" size={90} color="#636363" />
+          </div>
+        </a>
+      </section>
 
       <div className="inputnome">
         <input className="nome" type="text" placeholder="Nome" />
         <input className="sobrenome" type="text" placeholder="Sobrenome" />
       </div>
       <div className="inputDataCpf">
-       
         <input
           className="dataNascimento"
           type="text"
           placeholder="Data de Nascimento"
         />
-        <input className="Cpf" type="text" placeholder="CPF"/>
+        <input className="Cpf" type="text" placeholder="CPF" />
       </div>
       <div className="inputsInfos">
         <input className="Pais" type="text" placeholder="Pais" />
-        <input className="estado" type="text" placeholder="Estado"
-        />
+        <input className="estado" type="text" placeholder="Estado" />
       </div>
+      <a href="">
+        <div className="buttonProximo">Próximo</div>
+      </a>
+
+      <section className="linha">
+        <img src={Linha} alt="" />
+        <h5>OU</h5>
+        <img src={Linha} alt="" />
+      </section>
+
+      <section className="sociais">
+        <a href="">
+          <img src={Google} alt="" />
+        </a>
+        <a href="">
+          <img src={Facebook} alt="" />
+        </a>
+        <a href="">
+          <img src={Apple} alt="" />
+        </a>
+      </section>
+      <Footer />
     </Container>
   );
 }
