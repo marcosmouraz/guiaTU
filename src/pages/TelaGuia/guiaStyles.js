1e466a8f603e038961639226bcdba709e5c6efd8
Marcos Moura <marcosmoura1905@outlook.com>
2024-08-15 20:43:18 -0300

    fix: ajustes na tela guia

--- src/pages/TelaGuia/guiaStyles.js
+++ src/pages/TelaGuia/guiaStyles.js
@@ -2,58 +2,51 @@ import styled from "styled-components";
 
 export const Container = styled.body`
   width: 100%;
-  height: 70vh;
+  height: 180vh;
   max-width: 100%;
   margin: 0 auto;
 
   .titulo {
     color: #04136e;
     font-weight: 600;
-    font-size: 14px;
+    font-size: 20px;
     display: flex;
-    width: 60%;
+    width: 100%;
     justify-content: center;
     margin-top: 3rem;
-    margin-bottom: 2rem;
+    margin-bottom: 6rem;
+    gap: 20px;
+    align-items: center;
+    font-weight: 800;
+    white-space: nowrap;
+  }
+  .nomeRota {
+    font-size: 30px;
+    font-weight: 200;
+    color: #04136e;
   }
   .blocoUm {
     justify-content: space-around;
     display: flex;
-    height: 14rem;
     align-items: center;
   }
   .fotoPerfil {
-    height: 13rem;
+    height: 17rem;
+    padding-left: 4rem;
   }
   .fotoCidade {
-    height: 13.5rem;
-  }
-  .nomeRota {
-    position: absolute;
-    font-size: 25px;
-    font-weight: 700;
-    margin-bottom: 10.5rem;
-    margin-left: 6rem;
-    color: #04136e;
-  }
-  .nomeCidade {
-    position: absolute;
-    font-size: 18px;
-    font-weight: 400;
-    margin-top: 10.5rem;
-    color: #04136e;
+    height: 17.5rem;
   }
   .blocoDois {
     display: flex;
-    justify-content: space-around;
-    width: 100%;
-    gap: 5rem;
+    width: 77%;
     align-items: center;
+    gap: 3rem;
   }
   .nomeGuia {
     color: #04136e;
     font-size: 16px;
-    margin-right: 3.5rem;
+    margin: auto;
   }
   .frase {
     color: #04136e;
@@ -61,20 +54,47 @@ export const Container = styled.body`
   }
   .blocoTres {
     display: flex;
-    width: 35%;
-    justify-content: space-around;
+    padding-left: 7rem;
+  }
+  .infoGuia {
+    width: 100%;
+    padding-right: 20rem;
   }
   .credencial {
     text-align: center;
     font-size: 14px;
-    margin-top: 5px;
+    width: 30%;
+    padding-top: 10px;
+    right: 5rem;
   }
   .blocoQuatro {
     display: flex;
     justify-content: space-around;
     margin-top: 3rem;
     white-space: nowrap;
+    margin: 100px;
   }
+  .data {
+    font-size: 20px;
+    color: #04136e;
+  }
+  .valor {
+    font-size: 20px;
+    color: #04136e;
+  }
+
   .reserva {
+    display: flex;
+    justify-content: space-around;
+    padding-bottom: 2rem;
+    background-color: #9b9b9b9b;
+    height: 20rem;
+    width: 30%;
+    border-radius: 20px;
+    text-align: center;
+    padding-top: 1rem;
+  }
+  .reserva h4 {
+    font-size: 20px;
   }
 `;
